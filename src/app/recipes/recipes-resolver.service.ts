d6ab2241bb855b24194e44a502ddeeca2b7488a0
myat-tomato <tomato.202604.myat@gmail.com>
2026-07-21 17:03:01 +0900

    improved and recommeded resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,23 +1,36 @@
 import { Injectable, inject } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, Resolve, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
 import { RecipeService } from "./recipe.service";
 
-@Injectable({
-    providedIn: 'root'
-})
+//---Legacy way of making the resolver---
 
-export class RecipesResolverService implements Resolve<Recipe[]> {
-    private dataStorage = inject(DataStorageService);
-    private recipeService = inject(RecipeService);
+// @Injectable({
+//     providedIn: 'root'
+// })
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        const recipes = this.recipeService.recipes()
-        if(recipes.length === 0){
-            return this.dataStorage.fetchRecipes();
-        } else {
-            return recipes
-        }
+// export class RecipesResolverService implements Resolve<Recipe[]> {
+//     private dataStorage = inject(DataStorageService);
+//     private recipeService = inject(RecipeService);
+
+//     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+//         const recipes = this.recipeService.recipes()
+//         if(recipes.length === 0){
+//             return this.dataStorage.fetchRecipes();
+//         } else {
+//             return recipes
+//         }
+//     }
+// }
+
+//---Current recommended way of making the resolver---
+
+export const RecipesResolverService: ResolveFn<Recipe[]> = () => {
+    const recipes = inject(RecipeService).recipes();
+    if (recipes.length === 0) {
+        return inject(DataStorageService).fetchRecipes();
+    } else {
+        return recipes;
     }
 }
